d353db2eafdd24939f0932d91bd4fc4791c44347
ngphuc2808 <ngphuc.2808@gmail.com>
2025-04-03 13:57:25 +0700

    fix: cookie middleware

--- apps/web/app/api/guest/auth/refresh-token/route.ts
+++ apps/web/app/api/guest/auth/refresh-token/route.ts
@@ -32,20 +32,25 @@ export async function POST(request: Request) {
     const decodedRefreshToken = jwt.decode(payload.data.refreshToken) as {
       exp: number
     }
-    cookieStore.set('accessToken', payload.data.accessToken, {
+
+    const cookieOptions = {
       path: '/',
       httpOnly: true,
-      sameSite: 'lax',
-      secure: true,
-      expires: decodedAccessToken.exp * 1000,
-    })
-    cookieStore.set('refreshToken', payload.data.refreshToken, {
-      path: '/',
-      httpOnly: true,
-      sameSite: 'lax',
+      sameSite: 'none' as const,
       secure: true,
       expires: decodedRefreshToken.exp * 1000,
+      domain:
+        process.env.NODE_ENV === 'production'
+          ? process.env.NEXT_PUBLIC_COOKIE_DOMAIN
+          : undefined,
+    }
+
+    cookieStore.set('accessToken', payload.data.accessToken, {
+      ...cookieOptions,
+      expires: decodedAccessToken.exp * 1000,
     })
+    cookieStore.set('refreshToken', payload.data.refreshToken, cookieOptions)
+
     return Response.json(payload)
   } catch (error: any) {
     console.log('>>> error: ', error)
